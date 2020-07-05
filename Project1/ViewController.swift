6a34e8c20cd3359bb282007002fc3ab6c77a5cd0
Mahima Chawla <mahima.chawla@classpass.com>
2020-07-04 19:09:03 -0600

    First commit

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Disney's Two Cents"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path).sorted()
+        for item in items {
+            if item.hasSuffix("JPG"){
+                pictures.append(item)
+            }
+        }
+        print(pictures)
     }
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row].replacingOccurrences(of: ".JPG", with: "")
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
 }
 
